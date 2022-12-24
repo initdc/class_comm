010c69dfd2f2a27e4935fb87c48d065ff1c504ce
initdc <initd@outlook.com>
2022-12-24 13:16:56 +0000

    build: add base by `bundle gem class_comm`

---
+++ lib/class_comm/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ClassComm
+  VERSION = "0.1.0"
+end
---
+++ spec/class_comm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ClassComm do
+  it "has a version number" do
+    expect(ClassComm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
